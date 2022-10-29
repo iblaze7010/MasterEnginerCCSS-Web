80d3eaf646b74906880669a8fe6dbb6829ea706e
Rodrigo Sebastian Valdivia vela <valdiviavela@hotmail.com>
2022-10-28 20:42:33 -0500

    fix

--- js/Portada.js
+++ js/Portada.js
@@ -1,11 +1,13 @@
 document.addEventListener("DOMContentLoaded", function () {
   const introText = document.getElementsByClassName("intro__text"),
     introBtn = document.getElementsByClassName("intro__btn");
+    introBtn2 = document.getElementsByClassName("intro__btn2");
 
   introBtn[0].addEventListener("click", () => window.location='#Contacto');
 
   setTimeout(() => {
     introText[0].classList.add("start");
     introBtn[0].classList.add("start");
+    introBtn2[0].classList.add("start");
   }, 800);
 });
